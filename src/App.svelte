9b29129802b3581e13d61f44988f345236a22f51
Danil <danya.trofimov@gmail.com>
2021-11-28 18:03:00 +0200

    css

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
 
   let titleValue = '';
   let bodyValue = '';
-  let dateValue = '';
+  let deadlineValue = '';
 
   function createApolloClient() {
     const wslink = new WebSocketLink({
@@ -48,8 +48,14 @@
       alert('Title can`t be empty!');
       return;
     }
+    if (deadlineValue == '') {
+      await request.startExecuteMyMutation(
+        Operations.mutationInsertWithoutDeadline(titleValue, bodyValue),
+      );
+      return;
+    }
     await request.startExecuteMyMutation(
-      Operations.mutationInsert(titleValue, bodyValue, dateValue),
+      Operations.mutationInsert(titleValue, bodyValue, deadlineValue),
     );
   };
 
@@ -67,9 +73,9 @@
 <main>
   <!--{JSON.stringify($Tasks)}!-->
   {#if $Tasks.loading}
-    <h1>loading</h1>
+    <div class="loader" />
   {:else if $Tasks.error}
-    <h1>error</h1>
+    <h1 class="message">error</h1>
   {:else if $Tasks.data}
     <form class="form" on:submit|preventDefault={addTask}>
       <div class="form__section">
@@ -87,13 +93,13 @@
         />
         <input
           type="date"
-          body="date"
-          placeholder="date"
-          on:input={(event) => (dateValue = event.target.value)}
+          body="deadline"
+          placeholder="deadline"
+          on:input={(event) => (deadlineValue = event.target.value)}
         />
       </div>
+      <button>Add task</button>
     </form>
-    <button on:click={addTask}>Add task</button>
     <table border="1">
       <caption>ToDo</caption>
       <tr>
@@ -117,7 +123,11 @@
           <td>{task.noteTitle}</td>
           <td>{task.noteBody}</td>
           <td>{task.deadline}</td>
-          <td><button on:click={() => deleteTask(task.id)}>Delete</button></td>
+          <td
+            ><button class="delete" on:click={() => deleteTask(task.id)}
+              >Delete</button
+            ></td
+          >
         </tr>
       {/each}
     </table>
@@ -125,4 +135,125 @@
 </main>
 
 <style>
+  :root {
+    --background-color: lightblue;
+    --form-background-color: #fff;
+    --light-color: #eee;
+    --dark-color: #000;
+    --button-hover-color: gray;
+    --loader-color: gray;
+    --delete-color: rgb(163, 13, 13);
+    --table-color: #3e3cca;
+  }
+  main {
+    margin: 0;
+    max-width: 100%;
+    height: 100%;
+    background-color: var(--background-color);
+  }
+  .form {
+    background-color: var(--form-background-color);
+    border-radius: 5px;
+    padding: 20px;
+    width: 700px;
+    margin: auto;
+    display: flex;
+    gap: 20px;
+    position: relative;
+    border: 1px solid var(--light-color);
+  }
+  .form input,
+  .form button {
+    background-color: var(--light-color);
+    outline: none;
+    border: 1px solid var(--light-color);
+    border-radius: 3px;
+    padding: 10px;
+  }
+  .message {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+  }
+
+  table {
+    border-collapse: collapse;
+    margin: auto;
+    position: absolute;
+    width: 99%;
+    border-left: 3px solid var(--table-color);
+    border-right: 3px solid var(--table-color);
+    border-bottom: 3px solid var(--table-color);
+  }
+  caption {
+    background: var(--table-color);
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+    padding: 10px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.3);
+    color: var(--light-color);
+    font-family: 'Roboto Slab', serif;
+    font-style: normal;
+    font-size: 26px;
+    text-align: center;
+    margin: 0;
+  }
+  td,
+  th {
+    padding: 10px;
+  }
+  th {
+    text-align: left;
+    font-size: 18px;
+  }
+  tr:nth-child(2n) {
+    background: var(--light-color);
+  }
+  tr:nth-child(2n-1) {
+    background: var(--background-color);
+  }
+  td:last-of-type {
+    text-align: center;
+  }
+  .delete {
+    display: inline-block;
+    padding: 5px 10px;
+    background: var(--delete-color);
+    color: var(--light-color);
+    box-shadow: 2px 2px 0 0 var(--delete-color);
+    position: relative;
+  }
+  .delete:hover {
+    box-shadow: none;
+    top: 2px;
+    left: 2px;
+  }
+
+  .loader {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    width: 80px;
+    height: 80px;
+  }
+  .loader:after {
+    content: ' ';
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid;
+    border-color: var(--button-hover-color) transparent
+      var(--button-hover-color) transparent;
+    animation: loader 1.2s linear infinite;
+  }
+  @keyframes loader {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 </style>
--- src/helper/operations.js
+++ src/helper/operations.js
@@ -13,22 +13,37 @@ export class Operations {
     }
   }
 `;
-  static mutationDelete = (id) => `
-  mutation MyMutation {
+  static mutationInsertWithoutDeadline = (title, body) => `
+mutation MyMutation($deadline: timestamptz = "") {
+  insert_todo_pinkpanther(objects: {noteBody: "${body}", noteTitle: "${title}", done: false}) {
+    returning {
+      id
+      noteBody
+      noteTitle
+      done
+      deadline
+    }
+  }
+}
+`;
+  static mutationDelete = (id) =>
+    `mutation MyMutation {
     delete_todo_pinkpanther_by_pk(id: ${id}){
       id
     }
   }
 
 `;
-  static mutationChecked = (id, checked = true) =>
+  static mutationChecked = (id, checked) =>
     `mutation MyMutation {
-update_todo_pinkpanther_by_pk(pk_columns: {id: "${id}"}, _set: {done: ${checked}}) {
-  id
-  noteBody
-  noteTitle
-  done
-  deadline
-}
+      update_todo_pinkpanther_by_pk(pk_columns:
+      {id: "${id}"},
+      _set: {done: ${checked}}){
+      id
+      noteBody
+      noteTitle
+      done
+      deadline
+    }
 }`;
 }
